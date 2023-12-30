6564734f54e4597d3a53f32614b1e6f64d4d0ac4
Kye <kye@apacmediasolutions.com>
2023-12-30 11:03:59 -0500

    [CLEANUP][Tests]

--- ast_torch/model.py
+++ ast_torch/model.py
@@ -153,17 +153,3 @@ class ASTransformer(nn.Module):
 
         return x
 
-
-import torch
-from ast_torch.model import ASTransformer
-
-# Create dummy data
-x = torch.randn(2, 16)
-
-# Initialize model
-model = ASTransformer(
-    dim=4, seqlen=16, dim_head=4, heads=4, depth=2, patch_size=4
-)
-
-# Run model and print output shape
-print(model(x).shape)
